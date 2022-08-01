ae3143741f7a614fa055c989cb7a209b0b83e39a
Ravi Kumar Balija (rbalija) <rbalija@micron.com>
2022-08-01 14:19:45 +0530

    add ag-grid-basic-tut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef } from 'ag-grid-community';
+import { filter, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app-ag-grid';
+export class AppComponent implements OnInit{
+  rowData$!: Observable<any[]>;
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  colDefs: ColDef[] = [
+    {field: "make"},
+    {field: "model"},
+    {field: 'price'}
+  ];
+
+  defaultColDef: ColDef ={
+    sortable: true, filter: true } ;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.rowData$ = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  onCellClicked(event: CellClickedEvent){
+    console.log(event.data)
+    console.log(event.node)
+  }
+
+  clearSelection(){
+    this.agGrid.api.deselectAll();
+  }
 }
